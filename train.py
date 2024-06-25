c718ca2b9c13f20f2386ec37eb3efbaabd00075d
Jordan Deklerk <111652310+jordandeklerk@users.noreply.github.com>
2024-06-25 11:22:07 -0400

    fix train.py

--- train.py
+++ train.py
@@ -4,12 +4,11 @@ import torch.nn as nn
 import wandb
 import random
 import time
-import logging
 from src.utils.parser import args
 from src.utils.optimizer import AdamOptimizer
 
 def train(seed, model, x, n_epochs, n_obs, batch_size, print_output=False, lr=args.lr, weight_decay=args.weight_decay,
-           save_mse=False, save_mse_test=False, x_test=None, use_wandb=False):
+           save_mse=False, save_mse_test=False, x_test=None):
     """
     Trains a given model using the specified parameters and data.
 
@@ -25,7 +24,6 @@ def train(seed, model, x, n_epochs, n_obs, batch_size, print_output=False, lr=ar
     - save_mse (bool, optional): If True, saves the Mean Squared Error (MSE) on the training dataset after each epoch (default: False).
     - save_mse_test (bool, optional): If True, and if `x_test` is provided, saves the MSE on the test dataset after each epoch (default: False).
     - x_test (torch.Tensor, optional): The input data for testing to evaluate the model's performance (default: None).
-    - use_wandb (bool, optional): If True, uses wandb for logging (default: False).
 
     Returns:
     - model (torch.nn.Module): The trained model.
@@ -34,15 +32,14 @@ def train(seed, model, x, n_epochs, n_obs, batch_size, print_output=False, lr=ar
     - mse_saved (numpy.ndarray): MSE values for the training dataset for each epoch if `save_mse` is True; otherwise, an empty array.
     - mse_saved_test (numpy.ndarray): MSE values for the test dataset for each epoch if `save_mse_test` is True and `x_test` is provided; otherwise, an empty array.
     """
-    if use_wandb:
-        wandb.init(project="project_name", entity="entity_name")
-        wandb.config = {
-            "learning_rate": lr,
-            "epochs": n_epochs,
-            "batch_size": batch_size,
-            "seed": seed,
-            "weight_decay": weight_decay
-        }
+    # wandb.init(project="project_name", entity="entity_name")
+    # wandb.config = {
+    #     "learning_rate": lr,
+    #     "epochs": n_epochs,
+    #     "batch_size": batch_size,
+    #     "seed": seed,
+    #     "weight_decay": weight_decay
+    # }
 
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
@@ -57,7 +54,7 @@ def train(seed, model, x, n_epochs, n_obs, batch_size, print_output=False, lr=ar
     n_batches = max(n_obs // batch_size, 1)
 
     if print_output:
-        logging.info("Starting training VAE...")
+        print("Starting training...")
 
     model.train()
     loss_saved = np.empty(n_epochs * n_batches)
@@ -81,32 +78,28 @@ def train(seed, model, x, n_epochs, n_obs, batch_size, print_output=False, lr=ar
             loss.backward()
             optimizer.step()
 
-            if use_wandb:
-                wandb.log({"batch_loss": loss.item()})
+            # wandb.log({"batch_loss": loss.item()})
 
         if save_mse:
             mse = model.reconstruction_mse(x).item()
             mse_saved[epoch] = mse
 
-            if use_wandb:
-                wandb.log({"epoch": epoch, "mse_train": mse})
+            # wandb.log({"epoch": epoch, "mse_train": mse})
 
         if save_mse_test and x_test is not None:
             mse_test = model.reconstruction_mse(x_test).item()
             mse_saved_test[epoch] = mse_test
 
-            if use_wandb:
-                wandb.log({"epoch": epoch, "mse_test": mse_test})
+            # wandb.log({"epoch": epoch, "mse_test": mse_test})
 
         if epoch % 500 == 0 and print_output:
-            logging.info(f"\tEpoch: {epoch} \tLoss: {loss.item()}")
+            print(f"\tEpoch: {epoch} \tLoss: {loss.item()}")
             if save_mse:
-                logging.info(f"\tMSE: {mse_saved[epoch]}")
+                print(f"\tMSE: {mse_saved[epoch]}")
 
     end_time = time.time()
     run_time = end_time - start_time
 
-    if use_wandb:
-        wandb.log({"total_runtime": run_time})
+    # wandb.log({"total_runtime": run_time})
 
     return model, loss_saved, run_time, mse_saved, mse_saved_test
